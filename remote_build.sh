cfa78ea29c8dc4936b0b5ef35248934af2e35827
estebancqe <estebancqe@gmail.com>
2025-03-11 14:12:04 -0500

    remote_build

--- remote_build.sh
+++ remote_build.sh
@@ -5,7 +5,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-API_URL=https://creyente.onrender.com reflex export --frontend-only
+API_URL=https://esteban.onrender.com reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
 deactivate
